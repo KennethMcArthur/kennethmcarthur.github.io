2e6ca78b8ad41592cf471f4ec15e0fe551bc30a3
Kenneth <canowa@libero.it>
2021-05-26 09:52:34 +0200

    added the proper homepage for CS50x Final Project

---
+++ k_scripts.js
@@ -0,0 +1,62 @@
+
+// COLLAPSABLE SCRIPT
+let coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    //state of che parenthesis
+    this.classList.toggle("par-opened");
+
+    let openpar = this.querySelector("span.open-par");
+    let content = this.nextElementSibling;
+    let closingpar = content.nextElementSibling;
+
+    //curly braces behavior
+    if (this.classList.contains("par-opened")) {
+      openpar.classList.toggle("animated-par")
+      openpar.innerHTML = "{";
+      closingpar.style.display = "block";
+    } else {
+      openpar.innerHTML = "{ ... }"
+      openpar.classList.toggle("animated-par")
+      closingpar.style.display = "none";
+    }
+
+    //showing/hiding actual content
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+// END OF COLLAPSABLE SCRIPT
+
+// LOGO MOUSEOVER SCRIPT
+let logolist = document.getElementsByClassName("logo");
+var logonamefield = document.getElementById("logoname");
+
+for (i = 0; i < logolist.length; i++) {
+  logolist[i].addEventListener("mouseover", function() {
+    logonamefield.innerHTML = this.getAttribute("alt");
+  });
+
+  logolist[i].addEventListener("mouseout", function() {
+    logonamefield.innerHTML = "Here's the languages I use:";
+  });
+}
+// END OF LOGO MOUSEOVER SCRIPT
+
+
+// WINDOW RESIZE EVENT
+window.addEventListener("resize", function() {
+  //cerco tutti i collassabili aperti (cioè cone max-height diversa da 0) e ri-setto l'altezza
+  contentlist = document.getElementsByClassName("content");
+  for (i = 0; i < contentlist.length; i++) {
+    if (contentlist[i].style.maxHeight != 0) {
+      contentlist[i].style.maxHeight = contentlist[i].scrollHeight + "px";
+    }
+  }
+});
+// END OF WINDOW RESIZE EVENT
